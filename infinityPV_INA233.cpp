e6e967a9219e27be9082c42cf895a40ff66b4f65
TylerBird <tj.rose90@gmail.com>
2021-02-13 12:46:42 +0100

    use of any wire, remove warning for define comment

--- infinityPV_INA233.cpp
+++ infinityPV_INA233.cpp
@@ -23,15 +23,14 @@
 */
 /**************************************************************************/
 #if ARDUINO >= 100
- #include "Arduino.h"
+#include "Arduino.h"
 #else
- #include "WProgram.h"
+#include "WProgram.h"
 #endif
 
 #include <Wire.h>
 
 #include "infinityPV_INA233.h"
-
 
 /**************************************************************************/
 /*!
@@ -39,15 +38,14 @@
     PMBus comand (reg).
 */
 /**************************************************************************/
-void INA233::wireSendCmd(uint8_t reg)
-{
-  Wire.beginTransmission(ina233_i2caddr);
-  #if ARDUINO >= 100
-    Wire.write(reg);                       // PMBus command
-  #else
-    Wire.send(reg);                        // PMBus command
-  #endif
-  Wire.endTransmission();
+void INA233::wireSendCmd(uint8_t reg) {
+    m_I2C.beginTransmission(ina233_i2caddr);
+#if ARDUINO >= 100
+    m_I2C.write(reg);  // PMBus command
+#else
+    m_I2C.send(reg);           // PMBus command
+#endif
+    m_I2C.endTransmission();
 }
 /**************************************************************************/
 /*!
@@ -55,17 +53,16 @@ void INA233::wireSendCmd(uint8_t reg)
     by the PMBus comand (reg) over I2C
 */
 /**************************************************************************/
-void INA233::wireWriteByte (uint8_t reg, uint8_t value)
-{
-  Wire.beginTransmission(ina233_i2caddr);
-  #if ARDUINO >= 100
-    Wire.write(reg);                       // PMBus command
-    Wire.write(value);                     // byte to write
-  #else
-    Wire.send(reg);                        // PMBus command
-    Wire.send(value);                      // byte to write
-  #endif
-  Wire.endTransmission();
+void INA233::wireWriteByte(uint8_t reg, uint8_t value) {
+    m_I2C.beginTransmission(ina233_i2caddr);
+#if ARDUINO >= 100
+    m_I2C.write(reg);    // PMBus command
+    m_I2C.write(value);  // byte to write
+#else
+    m_I2C.send(reg);           // PMBus command
+    m_I2C.send(value);         // byte to write
+#endif
+    m_I2C.endTransmission();
 }
 /**************************************************************************/
 /*!
@@ -73,19 +70,18 @@ void INA233::wireWriteByte (uint8_t reg, uint8_t value)
     by the PMBus comand (reg) over I2C
 */
 /**************************************************************************/
-void INA233::wireWriteWord (uint8_t reg, uint16_t value)
-{
-  Wire.beginTransmission(ina233_i2caddr);
-  #if ARDUINO >= 100
-    Wire.write(reg);                       // PMBus command
-    Wire.write(value & 0xFF);              // Lower 8-bits
-    Wire.write((value >> 8) & 0xFF);       // Upper 8-bits
-  #else
-    Wire.send(reg);                        // PMBus command
-    Wire.send(value & 0xFF);               // Lower 8-bits
-    Wire.send(value >> 8);                 // Upper 8-bits
-  #endif
-  Wire.endTransmission();
+void INA233::wireWriteWord(uint8_t reg, uint16_t value) {
+    m_I2C.beginTransmission(ina233_i2caddr);
+#if ARDUINO >= 100
+    m_I2C.write(reg);                  // PMBus command
+    m_I2C.write(value & 0xFF);         // Lower 8-bits
+    m_I2C.write((value >> 8) & 0xFF);  // Upper 8-bits
+#else
+    m_I2C.send(reg);           // PMBus command
+    m_I2C.send(value & 0xFF);  // Lower 8-bits
+    m_I2C.send(value >> 8);    // Upper 8-bits
+#endif
+    m_I2C.endTransmission();
 }
 /**************************************************************************/
 /*!
@@ -94,16 +90,14 @@ void INA233::wireWriteWord (uint8_t reg, uint16_t value)
     block size (6 in this case), so the request must be for block_size+1
 */
 /**************************************************************************/
-void INA233::wireReadBlock(uint8_t reg, uint8_t value[6])
-{
-  int i;
-  uint8_t block_size;
-  Wire.requestFrom(ina233_i2caddr,(uint8_t)7,reg,(uint8_t)1,(uint8_t)true);
-  block_size=Wire.read();
-  for (i=0;i<block_size;i++)
-  {
-    value[i]=Wire.read();
-  }
+void INA233::wireReadBlock(uint8_t reg, uint8_t value[6]) {
+    int i;
+    uint8_t block_size;
+    m_I2C.requestFrom(ina233_i2caddr, (uint8_t)7, reg, (uint8_t)1, (uint8_t) true);
+    block_size = m_I2C.read();
+    for (i = 0; i < block_size; i++) {
+        value[i] = m_I2C.read();
+    }
 }
 
 /**************************************************************************/
@@ -111,21 +105,19 @@ void INA233::wireReadBlock(uint8_t reg, uint8_t value[6])
     @brief  Reads a 16 bit value from the INA233 over I2C
 */
 /**************************************************************************/
-void INA233::wireReadWord(uint8_t reg, uint16_t *value)
-{
-  Wire.requestFrom(ina233_i2caddr,(uint8_t)2,reg,(uint8_t)1,(uint8_t)true);
-  *value = Wire.read();
-  *value=((Wire.read() << 8) | *value);
+void INA233::wireReadWord(uint8_t reg, uint16_t *value) {
+    m_I2C.requestFrom(ina233_i2caddr, (uint8_t)2, reg, (uint8_t)1, (uint8_t) true);
+    *value = m_I2C.read();
+    *value = ((m_I2C.read() << 8) | *value);
 }
 /**************************************************************************/
 /*!
     @brief  Reads a 8 bit value from the INA233 over I2C
 */
 /**************************************************************************/
-void INA233::wireReadByte(uint8_t reg, uint8_t *value)
-{
-  Wire.requestFrom(ina233_i2caddr,(uint8_t)1,reg,(uint8_t)1,(uint8_t)true);
-  *value = Wire.read();
+void INA233::wireReadByte(uint8_t reg, uint8_t *value) {
+    m_I2C.requestFrom(ina233_i2caddr, (uint8_t)1, reg, (uint8_t)1, (uint8_t) true);
+    *value = m_I2C.read();
 }
 /**************************************************************************/
 /*!
@@ -138,120 +130,102 @@ void INA233::wireReadByte(uint8_t reg, uint8_t *value)
 
     */
 /**************************************************************************/
-uint16_t INA233::setCalibration(float r_shunt,float i_max,float *Current_LSB,float *Power_LSB, int16_t *mc,int8_t *Rc, int16_t *mp, int8_t *Rp,  uint8_t *ERROR)
-{
-  float C_LSB=0;
-  float P_LSB=0;
-  float CAL=0;
-  float m_c_F=0;
-  float m_p_F=0;
-  int32_t aux=0;
-  bool round_done=false;
-  int8_t local_R_c=0;
-  int8_t local_R_p=0;
-  uint8_t local_ERROR=0;
+uint16_t INA233::setCalibration(float r_shunt, float i_max, float *Current_LSB, float *Power_LSB, int16_t *mc, int8_t *Rc, int16_t *mp, int8_t *Rp, uint8_t *ERROR) {
+    float C_LSB = 0;
+    float P_LSB = 0;
+    float CAL = 0;
+    float m_c_F = 0;
+    float m_p_F = 0;
+    int32_t aux = 0;
+    bool round_done = false;
+    int8_t local_R_c = 0;
+    int8_t local_R_p = 0;
+    uint8_t local_ERROR = 0;
 
-  C_LSB=i_max/pow(2,15);
-  P_LSB=25*C_LSB;
-  *Current_LSB=C_LSB*1000000;
-  *Power_LSB=P_LSB*1000;
-  CAL=0.00512/(r_shunt*C_LSB);
+    C_LSB = i_max / pow(2, 15);
+    P_LSB = 25 * C_LSB;
+    *Current_LSB = C_LSB * 1000000;
+    *Power_LSB = P_LSB * 1000;
+    CAL = 0.00512 / (r_shunt * C_LSB);
 
-  //Check CAL is in the uint16 range
-  if (CAL>0xFFFF)
-    {
-      local_ERROR=1;
+    //Check CAL is in the uint16 range
+    if (CAL > 0xFFFF) {
+        local_ERROR = 1;
+    } else {
+        wireWriteWord(MFR_CALIBRATION, (uint16_t)CAL);
     }
-  else
-    {
-    wireWriteWord(MFR_CALIBRATION, (uint16_t)CAL);
-    }
-  m_c_F=1/C_LSB;
-  m_p_F=1/P_LSB;
+    m_c_F = 1 / C_LSB;
+    m_p_F = 1 / P_LSB;
 
-  //Calculate m and R for maximum accuracy in current measurement
-  aux=(int32_t)m_c_F;
-  while ((aux>32768)||(aux<-32768))
-    {
-      m_c_F=m_c_F/10;
-      local_R_c++;
-      aux=(int32_t)m_c_F;
+    //Calculate m and R for maximum accuracy in current measurement
+    aux = (int32_t)m_c_F;
+    while ((aux > 32768) || (aux < -32768)) {
+        m_c_F = m_c_F / 10;
+        local_R_c++;
+        aux = (int32_t)m_c_F;
     }
-  while (round_done==false)
-    {
-      aux=(int32_t)m_c_F;
-      if (aux==m_c_F)
-      {
-        round_done=true;
-      }
-      else
-      {
-         aux=(int32_t)(m_c_F*10);             //shift decimal to the right
-         if ((aux>32768)||(aux<-32768))       //m_c is out of int16 (-32768 to 32768)
-         {
-          round_done=true;
-         }
-         else
-         {
-          m_c_F=m_c_F*10;
-          local_R_c--;
-         }
-      }
+    while (round_done == false) {
+        aux = (int32_t)m_c_F;
+        if (aux == m_c_F) {
+            round_done = true;
+        } else {
+            aux = (int32_t)(m_c_F * 10);          //shift decimal to the right
+            if ((aux > 32768) || (aux < -32768))  //m_c is out of int16 (-32768 to 32768)
+            {
+                round_done = true;
+            } else {
+                m_c_F = m_c_F * 10;
+                local_R_c--;
+            }
+        }
     }
-  round_done=false;
-  //Calculate m and R for maximum accuracy in power measurement
-  aux=(int32_t)m_p_F;
-  while ((aux>32768)||(aux<-32768))
-    {
-      m_p_F=m_p_F/10;
-      local_R_p++;
-      aux=(int32_t)m_p_F;
+    round_done = false;
+    //Calculate m and R for maximum accuracy in power measurement
+    aux = (int32_t)m_p_F;
+    while ((aux > 32768) || (aux < -32768)) {
+        m_p_F = m_p_F / 10;
+        local_R_p++;
+        aux = (int32_t)m_p_F;
     }
-  while (round_done==false)
-    {
-      aux=(int32_t)m_p_F;
-      if (aux==m_p_F)
-      {
-        round_done=true;
-      }
-      else
-      {
-         aux=(int32_t)(m_p_F*10);          //shift decimal to the right
-         if ((aux>32768)||(aux<-32768))       //m_p is out of int16 (-32768 to 32768)
-         {
-          round_done=true;
-         }
-         else
-         {
-          m_p_F=m_p_F*10;
-          local_R_p--;
-         }
-      }
+    while (round_done == false) {
+        aux = (int32_t)m_p_F;
+        if (aux == m_p_F) {
+            round_done = true;
+        } else {
+            aux = (int32_t)(m_p_F * 10);          //shift decimal to the right
+            if ((aux > 32768) || (aux < -32768))  //m_p is out of int16 (-32768 to 32768)
+            {
+                round_done = true;
+            } else {
+                m_p_F = m_p_F * 10;
+                local_R_p--;
+            }
+        }
     }
-  *mp=m_p_F;
-  *mc=m_c_F;
-  *Rc=local_R_c;
-  *Rp=local_R_p;
-  *ERROR=local_ERROR;
+    *mp = m_p_F;
+    *mc = m_c_F;
+    *Rc = local_R_c;
+    *Rp = local_R_p;
+    *ERROR = local_ERROR;
 
-  m_c=int16_t(m_c_F);
-  m_p=int16_t(m_p_F);
-  R_c=local_R_c;
-  R_p=local_R_p;
+    m_c = int16_t(m_c_F);
+    m_p = int16_t(m_p_F);
+    R_c = local_R_c;
+    R_p = local_R_p;
 
-  return(uint16_t)CAL;
+    return (uint16_t)CAL;
 }
 /**************************************************************************/
 /*!
     @brief  Instantiates a new INA233 class
 */
 /**************************************************************************/
-INA233::INA233(uint8_t addr) {
-  ina233_i2caddr = addr;
-  m_c=0;
-  R_c=0;
-  m_p=0;
-  R_p=0;
+INA233::INA233(uint8_t addr, TwoWire &i2c) : m_I2C {
+    ina233_i2caddr = addr;
+    m_c = 0;
+    R_c = 0;
+    m_p = 0;
+    R_p = 0;
 }
 /**************************************************************************/
 /*!
@@ -259,7 +233,7 @@ INA233::INA233(uint8_t addr) {
 */
 /**************************************************************************/
 void INA233::begin() {
-  Wire.begin();
+    m_I2C.begin();
 }
 /**************************************************************************/
 /*!
@@ -268,10 +242,10 @@ void INA233::begin() {
 */
 /**************************************************************************/
 int16_t INA233::getBusVoltage_raw() {
-  uint16_t value;
-  wireReadWord(READ_VIN, &value);
+    uint16_t value;
+    wireReadWord(READ_VIN, &value);
 
-  return (int16_t)value;
+    return (int16_t)value;
 }
 /**************************************************************************/
 /*!
@@ -280,9 +254,9 @@ int16_t INA233::getBusVoltage_raw() {
 */
 /**************************************************************************/
 int16_t INA233::getShuntVoltage_raw() {
-  uint16_t value;
-  wireReadWord(MFR_READ_VSHUNT, &value);
-  return (int16_t)value;
+    uint16_t value;
+    wireReadWord(MFR_READ_VSHUNT, &value);
+    return (int16_t)value;
 }
 /**************************************************************************/
 /*!
@@ -291,9 +265,9 @@ int16_t INA233::getShuntVoltage_raw() {
 */
 /**************************************************************************/
 int16_t INA233::getCurrent_raw() {
-  uint16_t value;
-  wireReadWord(READ_IIN, &value);
-  return (int16_t)value;
+    uint16_t value;
+    wireReadWord(READ_IIN, &value);
+    return (int16_t)value;
 }
 /**************************************************************************/
 /*!
@@ -302,9 +276,9 @@ int16_t INA233::getCurrent_raw() {
 */
 /**************************************************************************/
 int16_t INA233::getPower_raw() {
-  uint16_t value;
-  wireReadWord(READ_PIN, &value);
-  return (int16_t)value;
+    uint16_t value;
+    wireReadWord(READ_PIN, &value);
+    return (int16_t)value;
 }
 /**************************************************************************/
 /*!
@@ -313,17 +287,16 @@ int16_t INA233::getPower_raw() {
 
 */
 /**************************************************************************/
-void INA233::getEnergy_raw(uint16_t* accumulator, uint8_t* roll_over, uint32_t* sample_count) {
-  uint8_t value[6];
-  //uint8_t test[6] = { 0x00, 0x11,0x22,0x33,0x44,0x55};
-  uint32_t aux;
-  wireReadBlock(READ_EIN, value);
-  *accumulator=(value[1] << 8) | value[0];
-  *roll_over=value[2];
-  *sample_count=uint32_t(value[5])<< 16;
-  *sample_count=((uint32_t(value[4])<< 8)| *sample_count);
-  *sample_count=(uint32_t(value[3])| *sample_count);
-
+void INA233::getEnergy_raw(uint16_t *accumulator, uint8_t *roll_over, uint32_t *sample_count) {
+    uint8_t value[6];
+    //uint8_t test[6] = { 0x00, 0x11,0x22,0x33,0x44,0x55};
+    uint32_t aux;
+    wireReadBlock(READ_EIN, value);
+    *accumulator = (value[1] << 8) | value[0];
+    *roll_over = value[2];
+    *sample_count = uint32_t(value[5]) << 16;
+    *sample_count = ((uint32_t(value[4]) << 8) | *sample_count);
+    *sample_count = (uint32_t(value[3]) | *sample_count);
 }
 /**************************************************************************/
 /*!
@@ -331,18 +304,18 @@ void INA233::getEnergy_raw(uint16_t* accumulator, uint8_t* roll_over, uint32_t* 
 */
 /**************************************************************************/
 float INA233::getAv_Power_mW() {
-  uint16_t accumulator=0;
-  uint8_t roll_over=0;
-  uint32_t sample_count=0;
-  uint32_t accumulator_24=0;
-  uint32_t raw_av_power=0;
-  float av_power=0;
-  getEnergy_raw(&accumulator,&roll_over, &sample_count);
-  accumulator_24=uint32_t(roll_over)*65536+uint32_t(accumulator);
-  raw_av_power=accumulator_24/sample_count;
+    uint16_t accumulator = 0;
+    uint8_t roll_over = 0;
+    uint32_t sample_count = 0;
+    uint32_t accumulator_24 = 0;
+    uint32_t raw_av_power = 0;
+    float av_power = 0;
+    getEnergy_raw(&accumulator, &roll_over, &sample_count);
+    accumulator_24 = uint32_t(roll_over) * 65536 + uint32_t(accumulator);
+    raw_av_power = accumulator_24 / sample_count;
 
-  av_power=(raw_av_power*pow(10,-R_p)-b_p)/m_p;
-  return av_power * 1000;
+    av_power = (raw_av_power * pow(10, -R_p) - b_p) / m_p;
+    return av_power * 1000;
 }
 /**************************************************************************/
 /*!
@@ -350,10 +323,10 @@ float INA233::getAv_Power_mW() {
 */
 /**************************************************************************/
 float INA233::getShuntVoltage_mV() {
-  uint16_t value=getShuntVoltage_raw();
-  float vshunt;
-  vshunt=(value*pow(10,-R_vs)-b_vs)/m_vs;
-  return vshunt * 1000;
+    uint16_t value = getShuntVoltage_raw();
+    float vshunt;
+    vshunt = (value * pow(10, -R_vs) - b_vs) / m_vs;
+    return vshunt * 1000;
 }
 
 /**************************************************************************/
@@ -362,10 +335,10 @@ float INA233::getShuntVoltage_mV() {
 */
 /**************************************************************************/
 float INA233::getBusVoltage_V() {
-  uint16_t value=getBusVoltage_raw();
-  float vbus;
-  vbus =(value*pow(10,-R_vb)-b_vb)/m_vb;
-  return vbus;
+    uint16_t value = getBusVoltage_raw();
+    float vbus;
+    vbus = (value * pow(10, -R_vb) - b_vb) / m_vb;
+    return vbus;
 }
 
 /**************************************************************************/
@@ -375,10 +348,10 @@ float INA233::getBusVoltage_V() {
 */
 /**************************************************************************/
 float INA233::getCurrent_mA() {
-  uint16_t value=getCurrent_raw();
-  float current;
-  current =(value*pow(10,-R_c)-b_c)/m_c;
-  return current*1000;
+    uint16_t value = getCurrent_raw();
+    float current;
+    current = (value * pow(10, -R_c) - b_c) / m_c;
+    return current * 1000;
 }
 
 /**************************************************************************/
@@ -388,9 +361,8 @@ float INA233::getCurrent_mA() {
 */
 /**************************************************************************/
 float INA233::getPower_mW() {
-  uint16_t value=getPower_raw();
-  float power;
-  power =(value*pow(10,-R_p)-b_p)/m_p;
-  return power*1000;
+    uint16_t value = getPower_raw();
+    float power;
+    power = (value * pow(10, -R_p) - b_p) / m_p;
+    return power * 1000;
 }
-
